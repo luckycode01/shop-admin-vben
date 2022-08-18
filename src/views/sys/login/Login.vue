d5f19711db9f40ecd8f4e23c491a9a588e5d41ab
13451167279 <1748595804@qq.com>
2022-08-19 00:59:09 +0800

    修改登录模块

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -32,24 +32,9 @@
         <div class="flex w-full h-full py-5 xl:h-auto xl:py-0 xl:my-0 xl:w-6/12">
           <div
             :class="`${prefixCls}-form`"
-            class="
-              relative
-              w-full
-              px-5
-              py-8
-              mx-auto
-              my-auto
-              rounded-md
-              shadow-md
-              xl:ml-16 xl:bg-transparent
-              sm:px-8
-              xl:p-4 xl:shadow-none
-              sm:w-3/4
-              lg:w-2/4
-              xl:w-auto
-              enter-x
-            "
+            class="relative w-full px-5 py-8 mx-auto my-auto rounded-md shadow-md xl:ml-16 xl:bg-transparent sm:px-8 xl:p-4 xl:shadow-none sm:w-3/4 lg:w-2/4 xl:w-auto enter-x"
           >
+            <h1>目前只有账号登陆功能可使用</h1>
             <LoginForm />
             <ForgetPasswordForm />
             <RegisterForm />
